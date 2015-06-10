06605793ad1c16603132de6114b48d7eb90dbd09
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2015-06-09 17:35:19 -0700

    Set up complete.

--- app.js
+++ app.js
@@ -7,26 +7,30 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/data', function (req, res) {
-    request('http://142.58.183.207:9200/jdbc/_search', {
-        method: 'GET',
-        
-        body: JSON.stringify({
-          "aggs" : {
-            "values" : {
-              "date_histogram" : {
-                "field" : "time",
-                "interval" : "1M",
-                "format" : "yyyy-MM-dd" 
-              },
-              "aggs": {
-                "consumption": {
-                  "avg": { "field": "value" }
-                }
-              }
-            }
+  req.query.interval
+
+  request('http://142.58.183.207:9200/jdbc/_search', {
+    method: 'GET',
+    
+    body: JSON.stringify({
+      "aggs" : {
+        "values" : {
+          "date_histogram" : {
+          "field" : "time",
+          "interval" : "1d",
+          "format" : "yyyy-MM-dd" 
+          },
+          "aggs": {
+          "consumption": {
+            "avg": { "field": "value" }
           }
-        })
-    }).pipe(res);
+          }
+        }
+      }
+    })
+
+  }).pipe(res);
 })
 
-app.listen(process.env.PORT);
+app.listen(3000);
+console.log('listening on port 3000');
