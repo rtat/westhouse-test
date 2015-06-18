606468fe1c29a456ce59befcc2642c99bc441afd
rtat <rosemarietat@gmail.com>
2015-06-18 16:05:19 -0700

    Practicing aggregations.

---
+++ practice.js
@@ -0,0 +1,44 @@
+var request = require('request');
+
+// value
+// time
+
+request('http://142.58.183.207:9200/jdbc/_search', {
+  method: 'GET',
+  
+  body: JSON.stringify({
+    // "aggs" : {
+    //   "values" : {
+    //     "date_histogram" : {
+    //     "field" : "time",
+    //     "interval" : "2d",
+    //     "format" : "yyyy/MM/dd" 
+    //     },
+    //     "aggs": {
+    //     "consumption": {
+    //       "avg": { "field": "value" }
+    //     }
+    //     }
+    //   }
+    // }
+    "aggs" : {
+      "values" : {
+        "date_histogram" : {
+        "field" : "time",
+        "interval" : "1d",
+        "format" : "yyyy/MM/dd" 
+        },
+        "aggs": {
+        "consumption": {
+          "avg": { "field": "value" }
+        }
+        }
+      }
+    }
+
+  })
+
+}, function (err, res, body) {
+  var result = JSON.stringify(JSON.parse(body.toString('utf8')), null, 2);
+  console.log(result);
+});
